7a44420ed618a6fb58a1c1ae61a32cbe5107700c
jogi295 <jogi.jo295@gmail.com>
2024-08-12 13:43:28 +0530

    added

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def subtract():
     return render_template('index.html', result=f"Subtraction Result: {result}")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) # commit
